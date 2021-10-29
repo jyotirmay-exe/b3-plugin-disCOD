8b6e55922d7c7fea8b7713bae4cb1585dad93d0d
Jyotirmay Kumar Jha <jyotirmay18.jk@gmail.com>
2021-10-29 12:53:00 +0530

    added auto promotion system based on kills

--- extplugins/discod.py
+++ extplugins/discod.py
@@ -1,45 +1,59 @@
 import b3
 import b3.events
 import b3.plugin
+import time
+import threading
 from datetime import datetime
 from MySQLdb import _mysql
-import functools
+from b3.clients import Group
 
-__version__ = "v1.0"
-__author__ = "someone"
 pluginInstance = None
 
 class DiscodPlugin(b3.plugin.Plugin):
 
     requiresConfigFile = True
-
-    def exceptionsHandler(f):
-        @functools.wraps(f)
-        def func(*args,**kwargs):
-            try:
-                return f(*args,**kwargs)
-            except Exception as ex:
-                global pluginInstance
-                pluginInstance.debug("ERROR in function %s : %s"%(f.__name__,ex))
-        return func
-
-    @exceptionsHandler
+    
     def onLoadConfig(self):
         self.debug("config loaded normal")
         self.debug("now loading config messages...")
-        self.min_level = int(self.config.get("settings","minimum_level"))
-        self.warn_senior = int(self.config.get("settings","warn_senior"))
+        #loading settings
+        self.min_level = int(self.config.getint("settings","min_level"))
+        self.warn_senior = int(self.config.getint("settings","warn_senior"))
+        self.send_eligible = int(self.config.getint("settings","send_eligible"))
+        self.min_interval = int(self.config.getint("settings","minInterval"))
+        self.autoPromote = int(self.config.getint("settings","auto_promote"))
+        self.invite_link = str(self.config.get("settings","invite_link"))
 
-        self.id_message = str(self.config.get("messages","id_message"))
-        self.warn_message = str(self.config.get("messages","warn_reason"))
-        self.success_message = str(self.config.get("messages","success_message"))
-        self.reattempt_message = str(self.config.get("messages","reattempt_message"))
-        self.notfound_message = str(self.config.get("messages","notfound_message"))
-        self.linktest_message = str(self.config.get("messages","linktest_message"))
-        self.linktest_pending_message = str(self.config.get("messages","linktest_pending_message"))
+        #loading kills required
+        self.reqKills = {}
+        for level in self.config.options("kills"):
+            self.reqKills[level] = (self.config.getint("kills",level))
+        self.reqKills["user"] = 0
+        self.reqKills["guest"] = 0
+
+        #loading responses
+        self.id_message = str(self.config.get("responses","id_message"))
+        self.warn_message = str(self.config.get("responses","warn_reason"))
+        self.success_message = str(self.config.get("responses","success_message"))
+        self.reattempt_message = str(self.config.get("responses","reattempt_message"))
+        self.notfound_message = str(self.config.get("responses","notFound_message"))
+        self.linktest_message = str(self.config.get("responses","linktest_message"))
+        self.linktest_pending_message = str(self.config.get("responses","linkPending_message"))
+        self.autoPromotion_message = str(self.config.get("responses","autoPromotion_message"))
+        self.autoPromotionEligible_message = str(self.config.get("responses","autoPromotionEligible_message"))
+        
+        #loading specific help docstrings for cmds
+        funcs = [func for func in dir(self) if func.startswith("_") is False]
+        for func in funcs:
+            try:
+                if func.startswith("cmd_"):
+                    f1 = getattr(self,func)
+                    f1.__func__.__doc__ = str(self.config.get("help",func))
+            except Exception as ex:
+                self.debug(ex)
+
         self.debug("config messages and settings loaded normal")
 
-    @exceptionsHandler
     def onStartup(self):
         global pluginInstance
         pluginInstance = self
@@ -58,18 +72,15 @@ class DiscodPlugin(b3.plugin.Plugin):
             self.debug('[UNLINK] command registered in the admin plugin')
             self._adminPlugin.registerCommand(self, 'linktest', self.min_level, self.cmd_linktest)
             self.debug('[LINKTEST] command registered in the admin plugin')
+            self.registerEvent(b3.events.EVT_CLIENT_AUTH)
             try:
                 self.console.storage._query("select * from discod;")
                 self.debug("located discod table")
             except _mysql.ProgrammingError as ex:
                 self.error("error locating `discod` table:")
                 self.error(ex)
-
-    @exceptionsHandler
+    
     def cmd_id(self,data,client,cmd=None):
-        """\
-        shows the B3 ID of the given player
-        """
         if not data:
             cmd.sayLoudOrPM(client, self.id_message.format(client_name = client.name, client_id = client.id))
         else:
@@ -77,12 +88,8 @@ class DiscodPlugin(b3.plugin.Plugin):
             client2 = self._adminPlugin.findClientPrompt(inp[0],client)
             if client2:
                 cmd.sayLoudOrPM(client, self.id_message.format(client_name = client2.name, client_id = client2.id))
-
-    @exceptionsHandler
+ 
     def cmd_link(self,data,client,cmd=None):
-        """\
-        ^3<8-digit-pin> ^0-^7 confirm the pin to link your discord account. If you do not have any pin, head over to ^0[^3v^0.^3F^0]^3's^7 discord and type !link <b3-id>
-        """
         if not data:
             client.message('^7Incorrect command syntax. ^3!link <8-digit-pin>^7 or type ^2!help link')
             return False
@@ -90,7 +97,7 @@ class DiscodPlugin(b3.plugin.Plugin):
         cursor = self.console.storage._query("select * from discod where b3_id = %s;"%(str(client.id)))
         rows = cursor.getOneRow()
         if not rows:
-            client.message(self.notfound_message)
+            client.message(self.notfound_message.format(invite=self.invite_link))
         else:
             pin = rows['pass']
             linked = int(rows['linked'])
@@ -110,6 +117,13 @@ class DiscodPlugin(b3.plugin.Plugin):
                             dc_tag+=ch
                 self.debug("@%s linked to %s"%(client.id,dc_tag))
                 cmd.sayLoudOrPM(client,self.success_message.format(id = client.id, dc_tag = dc_tag))
+                promotion = self.getPromotion(client=client)
+                if promotion is None:
+                    return
+                else:
+                    thread = threading.Thread(target=self.promoteClient,args=(client,promotion),)
+                    thread.start()
+
             else:
                 if linked == 1:
                     client.message(self.reattempt_message)
@@ -122,29 +136,21 @@ class DiscodPlugin(b3.plugin.Plugin):
                     else:
                         self._adminPlugin.warnClient(client,self.warn_message)
 
-    @exceptionsHandler
     def cmd_unlink(self, data, client, cmd = None):
-        """\
-        y - confirm unlinking your discord account
-        """
         if not data:
             client.message('^7Incorrect command syntax. ^3!unlink y^7')
             return False
         if str(data) == 'y':
             cursor = self.console.storage._query("select * from discod where b3_id = %s;"%(str(client.id)))
-            rows = cursor.getOneRow()
-            if len(rows) == 0:
-                client.message(self.notfound_message)
+            rows = cursor.getRow()
+            if rows == {}:
+                client.message(self.notfound_message.format(invite=self.invite_link))
             else:
                 self.console.storage._query("delete from discod where b3_id = %s;"%(str(client.id)))
                 cmd.sayLoudOrPM(client,"Unlinked your B3 ID from discod.")
                 self.debug("@%s unlinked"%(client.id))
-    
-    @exceptionsHandler
+     
     def cmd_linktest(self,data,client,cmd=None):
-        """\
-        check whether your b3 id is linked to a discord account
-        """
         if not data:
             self.getLinkStatus(client = client, cmd = cmd)
         else:
@@ -153,14 +159,14 @@ class DiscodPlugin(b3.plugin.Plugin):
             if client2:
                 self.getLinkStatus(client = client2, callerClient = client, cmd = cmd)
     
-    @exceptionsHandler
+    
     def getLinkStatus(self,client,callerClient=None,cmd=None):
         if not callerClient:
             callerClient = client
         cursor = self.console.storage._query("select * from discod where b3_id = %s;"%(str(client.id)))
         rows = cursor.getOneRow()
         if not rows:
-            callerClient.message(self.notfound_message)
+            callerClient.message(self.notfound_message.format(invite=self.invite_link))
         else:
             if int(rows['linked']) == 0:
                 callerClient.message(self.linktest_pending_message)
@@ -174,3 +180,90 @@ class DiscodPlugin(b3.plugin.Plugin):
                             dc_tag+=ch
                 time = (datetime.fromtimestamp(int(rows['linktime'])).strftime("%Y/%m/%d %H:%M:%S"))
                 cmd.sayLoudOrPM(callerClient,self.linktest_message.format(id=client.id, dc=dc_tag, time = time))
+
+    def isLinked(self,client):
+        res = self.console.storage._query("select * from discod where b3_id = %s;"%client.id).getRow()
+        if res == {}:
+            return False
+        else:
+            if res["linked"]==1:
+                return True
+            else:
+                return False
+    
+    def isDemoted(self,client):
+        try:
+            res = self.console.storage._query("select * from demotions where client_id = %s;"%client.id).getRow()
+            if res == {}:
+                return False
+            else:
+                if res["inactive"]==1:
+                    return False
+                else:
+                    return True
+        except _mysql.ProgrammingError:
+            #in case the demotions table doesn't exist
+            return False
+
+    def getKills(self,client):
+        res = self.console.storage._query("select * from xlr_playerstats where client_id = %s;"%client.id).getRow()
+        if res=={}:
+            return None
+        else:
+            return res["kills"]
+
+    def getPromotion(self,client):
+        cGroup = client.maxGroup.keyword
+        cKills = self.getKills(client)
+
+        if self.isDemoted(client):
+            return
+        
+        reqKills = sorted(self.reqKills.items(), key = lambda x:x[1])
+
+        if cKills > self.reqKills[cGroup]:
+            ind = None
+            for i in range(0,len(reqKills)):
+                if reqKills[i][0]==cGroup:
+                    ind = i
+
+        killsGroup = None
+
+        for i in range(ind,len(reqKills)):
+            if cKills>=reqKills[i][1]:
+                killsGroup = reqKills[i][0]
+            else:
+                break
+
+        group = Group(keyword=killsGroup)
+
+        if group.keyword != cGroup:
+            newGroup = self.console.storage.getGroup(group)
+            return newGroup
+        else:
+            return None
+    
+    def promoteClient(self,client,group):
+        time.sleep(5)
+        if not self.isLinked(client):
+            if self.send_eligible==0:
+                return
+            timeDiff = int(time.time() - client.lastVisit)
+            if timeDiff < self.min_interval:
+                self.debug("not sending link reminder to @%s cuz client was last seen only %s seconds ago"%(client.id,timeDiff))
+                return
+            client.message(self.autoPromotionEligible_message.format(groupname=group.name,grouplevel=group.level,invite=self.invite_link))
+        else:
+            client.setGroup(group)
+            client.save()
+            client.message(self.autoPromotion_message.format(groupname=group.name,grouplevel=group.level))
+        
+    def onEvent(self,event):
+        if event.type == b3.events.EVT_CLIENT_AUTH:
+            if self.autoPromote==1:
+                promotion = self.getPromotion(client=event.client)
+                if promotion is None:
+                    return
+                else:
+                    thread = threading.Thread(target=self.promoteClient,args=(event.client,promotion),)
+                    thread.start()
